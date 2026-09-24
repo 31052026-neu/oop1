004fa2b64247f524e5ab12d8f3993913919d443d
31052026-neu <joshua_moore@teilnehmer.cdemy.de>
2026-09-24 16:21:13 +0200

    oop4_fertig

--- main.dart
+++ main.dart
@@ -1,9 +1,94 @@
+import 'dart:math';
+
 import 'teilnehmer.dart';
 
 void main() {
-  final teilnehmerDaten = Teilnehmer1('Joshua', 'Moore', 30, 2);
-  final teilnehmer2Daten = Teilnehmer1('Harry', 'Kane', 2, 3);
+  final dartKurs = Kurs('Dart');
+  final javaScriptKurs = Kurs('Javascript');
+  final pythonKurs = Kurs('Python');
 
-  print(teilnehmerDaten);
-  print(teilnehmer2Daten);
+  final teilnehmer1 = Teilnehmer(
+    'Joshua',
+    'Moore',
+    30,
+    Geschlecht.maenlich,
+    1,
+    randomGenerator(),
+  );
+  final teilnehmer2 = Teilnehmer(
+    'Lukas',
+    'Nies',
+    26,
+    Geschlecht.maenlich,
+    2,
+    randomGenerator(),
+  );
+  final teilnehmer3 = Teilnehmer(
+    'Leonie',
+    'Müller',
+    22,
+    Geschlecht.weiblich,
+    2,
+    randomGenerator(),
+  );
+  dartKurs.teilnehmer.addAll([teilnehmer1, teilnehmer2, teilnehmer3]);
+  javaScriptKurs.teilnehmer.addAll([teilnehmer2, teilnehmer3, teilnehmer1]);
+  pythonKurs.teilnehmer.addAll([teilnehmer2, teilnehmer1, teilnehmer3]);
+
+  print(javaScriptKurs.teilnehmer);
+  print('Zugriffsberechtigung Joshua Moore: ${randomGenerator()}');
+  print('Zugriffsberechtigung Lukas Nies: ${randomGenerator()}');
+  print('Zugriffsberechtigung Leonie Müller: ${randomGenerator()}');
+}
+
+// zurück geben eine zeichenkette sprich: String (besteht aus Ziefern und  Buchstaben)
+// zeihenkette soll 9 zeichen lang sein
+// zeichen in zeichenkette soll einzigartig sein
+
+String randomGenerator() {
+  List<String> zeichen = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+  ];
+  String result = '';
+  for (int i = 0; i < 9; i++) {
+    int rundomIndex = Random().nextInt(zeichen.length);
+    result = result + "${zeichen[rundomIndex]}";
+  }
+
+  return result;
 }
--- teilnehmer.dart
+++ teilnehmer.dart
@@ -1,12 +1,34 @@
-class Teilnehmer1 {
+import 'dart:math';
+
+class Zugriffsberechtigung {}
+
+enum Geschlecht { maenlich, weiblich }
+
+class Teilnehmer {
   String? vorName;
   String? nachName;
   int? alter;
+  Geschlecht geschlecht;
   int? abschlussnote;
+  String zugriffsberechtigung;
 
-  Teilnehmer1(this.vorName, this.nachName, this.alter, this.abschlussnote);
+  Teilnehmer(
+    this.vorName,
+    this.nachName,
+    this.alter,
+    this.geschlecht,
+    this.abschlussnote,
+    this.zugriffsberechtigung,
+  );
   @override
   String toString() {
-    return '${vorName ?? ""} ${nachName ?? ""}, ist ${alter ?? ""} alt und hat folgende Abschlussnote: ${abschlussnote ?? ""}.';
+    return 'Kurs:  ${vorName ?? ""} ${nachName ?? ""}, ist ${alter ?? ""} alt, ${geschlecht.name}, hat folgende Abschlussnote: ${abschlussnote ?? ""}. ';
   }
+}
+
+class Kurs {
+  String? kursName;
+  List<Teilnehmer> teilnehmer = [];
+
+  Kurs(this.kursName);
 }
